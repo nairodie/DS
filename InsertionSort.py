cff865d7afda6bcc41541fa04c835ae6d9ed9bd9
nairodie <163676344+nairodie@users.noreply.github.com>
2024-03-17 11:59:21 -0500

    Create InsertionSort.py

---
+++ InsertionSort.py
@@ -0,0 +1,21 @@
+def insertion_sort(numbers):
+    for i in range(1, len(numbers)):
+        j = i
+        while j > 0 and numbers[j] < numbers[j - 1]:
+            temp = numbers[j]
+            numbers[j] = numbers[j - 1]
+            numbers[j - 1] = temp
+            j = j - 1
+
+
+numbers = [10, 2, 78, 4, 45, 32, 7, 11]
+print('UNSORTED:', end = ' ')
+for num in numbers:
+    print(num, end = ' ')
+print()
+
+insertion_sort(numbers)
+print('SORTED', end = ' ')
+for num in numbers:
+    print(num, end = ' ')
+print()
